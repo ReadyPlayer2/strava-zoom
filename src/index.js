2045af21a5fd8777f238fbe3453b396677f83111
Jack Dunning <jackdunning23@gmail.com>
2019-12-28 20:09:14 +0000

    strava-zoom branding + init routing + show button

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router basename={process.env.PUBLIC_URL}>
+        <div>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route component={() => (<div>404 Not found.</div>)} />
+            </Switch>
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
