94535828a87832b9c370bededc60f60d5a5b078b
Jack Dunning <jackdunning23@gmail.com>
2019-12-28 20:11:38 +0000

    function for button press

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 
 class App extends Component {
   
+  connectWithStrava = async () => {
+    console.log("Connecting to Strava...");
+
+    // client_id for my Strava application as listed at https://www.strava.com/settings/api when logged in
+    window.location.replace('https://www.strava.com/oauth/authorize?client_id=41064&response_type=code&redirect_uri=http://localhost:3000&approval_prompt=force&scope=activity:read');
+  }
+  
   render() {
     return (
       <div className="App">
